a75a1481559ab09e643da838d63643152c6356bb
boncpes <78350722+boncpes@users.noreply.github.com>
2021-03-25 17:52:41 +0700

    final

--- Silk Texture.c
+++ Silk Texture.c
@@ -1,20 +1,41 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-    int m,n,p,q,r,i,j,k;
+    int m,n,p,q,r,i,j,k,h;
     scanf("%d",&m);
     char s[m][70];
-    for(i=0;i<m;i++)
+    scanf("%d",&n);
+    for(i=0; i<m; i++)
     {
-        for(j=0;j<70;j++)
+        for(j=0; j<70; j++)
         {
-            s[p][j]='o';
+            s[i][j]='o';
         }
     }
-    scanf("%d",&n);
-    for(i=0;i<m;i++)
+    for(i=0; i<n; i++)
     {
-        printf("%s\n",s[i]);
+        scanf("%d %d %d",&p,&q,&r);
+        p-=1;
+        q-=1;
+        for(k=0; k<70; k++)
+        {
+            if(k==q)
+            {
+                for(h=k; h<(70-(70-q))+r; h++)
+                {
+                    s[p][h]='x';
+                }
+            }
+        }
+    }
+    for(i=0; i<m; i++)
+    {
+        for(j=0; j<70; j++)
+        {
+            printf("%c",s[i][j]);
+        }
+        printf("\n");
     }
     return 0;
 }
