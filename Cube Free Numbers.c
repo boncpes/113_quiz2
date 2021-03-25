5757ad410c992da40871f303f2d8e0658aaa54df
boncpes <78350722+boncpes@users.noreply.github.com>
2021-03-25 19:00:09 +0700

    final 2

--- Cube Free Numbers.c
+++ Cube Free Numbers.c
@@ -1,46 +1,15 @@
 #include<stdio.h>
-int cube(int m)
+int cube(int m,int n)
 {
-    if(m%(2*2*2)==0)
+    int i;
+    for(i=2;i<=n;i++)
     {
-        return 1;
+        if(m%(i*i*i)==0)
+        {
+            return 1;
+        }
     }
-    else if(m%(3*3*3)==0)
-    {
-        return 1;
-    }
-    else if(m%(4*4*4)==0)
-    {
-        return 1;
-    }
-    else if(m%(5*5*5)==0)
-    {
-        return 1;
-    }
-    else if(m%(6*6*6)==0)
-    {
-        return 1;
-    }
-    else if(m%(7*7*7)==0)
-    {
-        return 1;
-    }
-    else if(m%(8*8*8)==0)
-    {
-        return 1;
-    }
-    else if(m%(9*9*9)==0)
-    {
-        return 1;
-    }
-    else if(m%(10*10*10)==0)
-    {
-        return 1;
-    }
-    else
-    {
-        return 2;
-    }
+    return 2;
 }
 int main()
 {
@@ -49,26 +18,28 @@ int main()
     int cf[100000]= {0};
     cf[0]=1;
     k=1;
-    t=cube(n);
-    if(c==2)
+    for(i=2;i<1000;i++)
     {
-        for(i=2; i<n*n; i++)
+        c=cube(i,n);
+        if(c==1)
         {
-            c=cube(i);
-            if(c==1)
-            {
-            }
-            else if(c==2)
-            {
-                ans++;
-                if(n==i)
-                {
-                    printf("%d",ans);
-                }
-            }
+        }
+        else if(c==2)
+        {
+            cf[k]=i;
+            k++;
         }
     }
-    else if(t==1)
+     j=1;
+    for(i=0;i<n;i++)
+    {
+        if(n==cf[i])
+        {
+            printf("%d",i+1);
+            j=2;
+        }
+    }
+    if(j==1)
     {
         printf("Not Cube Free");
     }
