3396712de836f6b3e3ef363119cd8c411d0a21c0
boncpes <78350722+boncpes@users.noreply.github.com>
2021-03-25 20:00:05 +0700

    final end

--- Palindrome.c
+++ Palindrome.c
@@ -1,73 +1,29 @@
 #include<stdio.h>
 int main()
 {
-    int n,m,i,j,k,ans=0;
-    scanf("%d",&n);
-    if(n/10==0 || n/10>0)
+    int n,m=0,i,j,k,ans=0;
+    scanf("%d",n);
+    m=0;
+    j=0;
+    for(i=1; i<=9; i++)
     {
-        if(n/10>1 || n%10==0)
-        {
-            ans=9;
-            int se[9]= {11,22,33,44,55,66,77,88,99};
-            for(i=9; i<=0; i++)
-            {
-                if((n/10)==(se[i]/10))
-                {
-                    ans++;
-                }
-                ans--;
-            }
-        }
-        else
-        {
-            ans=n-1;
-        }
-    }
-    else if(n/100==0 || n/100==1)
-    {
-        printf("aa100\n");
-        int se[3];
-        ans+=18;
-        for(i=9; i<0; i++)
-        {
-            se[0]=i;
-            se[2]=i;
-            for(j=9; j<=0; j++)
-            {
-                se[1]=j;
-                m=(se[0]*100)+(se[1]*10)+(se[2]*1);
-                if(n-m==1)
-                {
-                    ans=18;
-                }
-                else
-                {
-                    n--;
-                }
-                ans++;
-            }
-        }
-        if(n%100==0)
+        if((n/10)==i)
         {
             ans=18;
         }
+        m=i*100;
+        m+=j*10;
+        m+=i;
+        ans++;
+        if(i==9)
+        {
+            j++;
+            i=1;
+            if(j==10)
+            {
+                break;
+            }
+        }
     }
-    else if(n/1000==0 || n/1000==1)
-    {
-
-    }
-    else if(n/10000==0 || n/10000==1)
-    {
-
-    }
-    else if(n/100000==0 || n/100000==1)
-    {
-
-    }
-    else if(n/1000000==0 || n/1000000==1)
-    {
-
-    }
-    printf("%d",ans);
     return 0;
 }
